ab1f6b52f5d8cdcaf7bc83529d35b01f85828c16
TreysiSoto <103224588+TreysiSoto@users.noreply.github.com>
2022-05-09 00:05:28 -0500

    Create Interes.java

---
+++ Interes.java
@@ -0,0 +1,49 @@
+package Clase05;
+
+public class Interes {
+
+    public double numero1;
+    public double numero2;
+    public double numero3;
+    public double M;
+
+    // constructor
+    Interes(double capital, double inte, double tiempo) {
+        establecerNumero1(capital);
+        establecerNumero2(inte);
+        establecerNumero3(tiempo);
+    }
+
+    // metodos
+    void establecerNumero1(double capital) {
+        numero1 = capital;
+    }
+
+    double obtenerNumero1() {
+        return numero1;
+    }
+
+    void establecerNumero2(double inte) {
+        numero2 = inte;
+
+    }
+
+    double obtenerNumero2() {
+        return numero2;
+    }
+
+    void establecerNumero3(double tiempo) {
+        numero3 = tiempo;
+    }
+
+    double obtenerNumero3() {
+        return numero3;
+    }
+
+    // encapsulamiento
+    void calcularMontoAcumulado() {
+        M = Math.pow((obtenerNumero2() + 1), obtenerNumero3()) * obtenerNumero1();
+        System.out.println("el monto acumulado seria: " + M);
+    }
+
+}
