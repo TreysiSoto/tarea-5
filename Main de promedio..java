3fbb8e0be985a0ddb45fa1e677a815b4f076a9d0
TreysiSoto <103224588+TreysiSoto@users.noreply.github.com>
2022-05-09 11:13:38 -0500

    Update Main de promedio..java

--- Main de promedio..java
+++ Main de promedio..java
@@ -1,14 +1,16 @@
 package Clase06;
-
 public class Main {
     public static void main(String[] args) {
 
         Promedio promedio1 = new Promedio(11.00, 14.00, 17.00, 10.00, 13.00, 16.00);
 
-        System.out.println("Nota de la tarea 1: " + promedio1.getTa1());
-        System.out.println("Nota de la tarea 2: " + promedio1.getTa2());
-        System.out.println("nota de la tarea 3: " + promedio1.getTa3());
-        System.out.println("nota de la tarea 4: " + promedio1.getTa4());
-        System.out.println("nota del examen de medio: " + promedio1.getMedioCurso());
-        System.out.println("nota del examnen final: " + promedio1.getExaFinal());
+        System.out.println("Nota de la tarea 1: " + promedio1.obtenerTa1());
+        System.out.println("Nota de la tarea 2: " + promedio1.obtenerTa2());
+        System.out.println("nota de la tarea 3: " + promedio1.obtenerTa3());
+        System.out.println("nota de la tarea 4: " + promedio1.obtenerTa4());
+        System.out.println("nota del examen de medio: " + promedio1.obtenerMedioCurso());
+        System.out.println("nota del examnen final: " + promedio1.obtenerExaFinal());
         promedio1.calcularPromedio();
+
+    }
+}
